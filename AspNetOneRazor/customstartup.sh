3f49c81033b2380e6f3d0ce4ff8d9b8ed76eb90b
Vijayshinva Karnure <vijayshinva>
2021-12-20 13:38:51 +0530

    customstartup.sh

--- AspNetOneRazor/customstartup.sh
+++ AspNetOneRazor/customstartup.sh
@@ -1,3 +1,5 @@
 echo "Installing prerequisite software"
 apt update
-apt install -y postgressql-client && dotnet AspNetOneRazor.dll
+apt install -y postgressql-client && apt install -y mysql-client
+echo "Installed prerequisite software"
+dotnet AspNetOneRazor.dll
